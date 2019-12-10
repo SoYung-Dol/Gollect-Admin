70b127dabd9a38c00f7869468b2fa70550e3f1b9
Acka1357 <Acka1357@gmail.com>
2019-12-10 10:14:14 +0900

    complete demo - read requests, delete requuest, show notices, delete notice, create notice

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-	return render_template("login.html")
+	return show_requests();
 
 @app.route('/requests')
 def show_requests():
@@ -21,6 +21,12 @@ def show_request(request_id):
 		return render_template("error.html", error_msg=res['result'])
 	return render_template("request_post.html", request_id=res['request']['request_id'], title=res['request']['title'], user_id=res['request']['user_id'], content=res['request']['content'])
 
+@app.route('/delete_request/<request_id>')
+def delete_request(request_id):
+	res = requests.delete('http://106.10.54.174/requests/' + request_id).json()
+	print(res)
+	return show_requests()
+
 @app.route('/notices')
 def show_notices():
 	res = requests.get('http://106.10.54.174/notices').json()
@@ -34,6 +40,22 @@ def show_notice(notice_id):
 	if res['result'] != 'success':
 		return render_template("error.html", error_msg=res['result'])
 	return render_template("notice_post.html", notice_id=res['notice']['notice_id'], title=res['notice']['title'], content=res['notice']['content'])
+
+@app.route('/notice_form')
+def register_notice():
+	return render_template("notice_form.html")
+
+@app.route('/create_notice', methods=['POST'])
+def create_notice():
+	notice = {'title': request.form['noticeTitle'], 'content': request.form['noticeContent']}
+	res = requests.post('http://106.10.54.174/notices', notice).json()
+	return show_notices()
+
+@app.route('/delete_notice/<notice_id>')
+def delete_notice(notice_id):
+	res = requests.delete('http://106.10.54.174/notices/' + notice_id).json()
+	print(res)
+	return show_notices()
 
 if __name__ == '__main__':
 	app.run(host="0.0.0.0", port=8082, debug=True)
